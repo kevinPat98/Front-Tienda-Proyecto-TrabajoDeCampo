8fb8e9cd7d7ac80052f54248976ccfae925db7ee
Kevin Patiño <57466420+nico19989@users.noreply.github.com>
2022-11-06 20:48:54 -0500

    Integra

---
+++ src/app/components/static/mongui/mongui.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-embajadoras',
+  templateUrl: './mongui.component.html',
+  styleUrls: ['./mongui.component.css']
+})
+export class EmbajadorasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
